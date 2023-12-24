29feced5bf44ddc2800630c477b27bfe0bdaaa17
hieukhocnhe <trantrunghieu422@gmail.com>
2023-12-25 00:27:51 +0700

    [CRUD] Delete course

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -5,6 +5,7 @@ async function connect() {
         await mongoose.connect('mongodb://localhost:27017/hieu_blog_dev', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
+            useCreateIndex: true,
         });
         console.log('Connect successfully !');
     } catch (error) {
